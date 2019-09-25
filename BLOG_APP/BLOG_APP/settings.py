53459536e3158032fb471d9b6bfb4608e12f546c
pnabiniw <pnabin@insightworkshop.io>
2019-09-26 00:55:17 +0545

    [Complete] Completion of CRUD on posts

--- BLOG_APP/BLOG_APP/settings.py
+++ BLOG_APP/BLOG_APP/settings.py
@@ -122,7 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
-CRISPY_TEMPLATE_FORM = 'bootstrap4'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
