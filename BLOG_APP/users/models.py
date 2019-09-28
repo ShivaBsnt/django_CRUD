bba43c9c7a705f22a009daef47eca142e09d6cf0
ShivaBsnt <bbang2358@gmail.com>
2019-09-28 12:04:46 +0545

    [Update] use of try except while fetching image url

--- BLOG_APP/users/models.py
+++ BLOG_APP/users/models.py
@@ -12,9 +12,11 @@ class Profile(models.Model):
 
     def save(self):
         super().save()
-
-        img = Image.open(self.image.path)
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+        try:
+            img = Image.open(self.image.path)
+            if img.height > 300 or img.width > 300:
+                output_size = (300, 300)
+                img.thumbnail(output_size)
+                img.save(self.image.path)
+        except:
+            pass
